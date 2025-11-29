578eb088930f29da75c70e5a8c54dbf9fc2868f5
NikoMyoji <nicolas.vlte@gmail.com>
2025-11-29 13:37:16 +0100

    Ajoutez des fichiers projet.

---
+++ AdventOfCode2025/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
