ef9b6da4c44a95ad5ccacb40611a19498abe638a
NikoMyoji <nicolas.vlte@gmail.com>
2025-12-01 11:03:18 +0100

    Structure projet

---
+++ AdventOfCode2025/Utilities/ISolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2025.Utilities
+{
+    interface ISolver
+    {
+        void ReadInputFile();
+        long GetSolution1Star();
+        long GetSolution2Star();
+    }
+}
---
+++ AdventOfCode2025/Utilities/Solver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2025.Utilities
+{
+    internal abstract class Solver : ISolver
+    {
+        protected string _input ="";
+        protected bool _verbose;
+        public virtual long GetSolution1Star()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Not Implemented");
+            Console.ResetColor();
+            return -1;
+        }
+
+
+
+        public virtual long GetSolution2Star()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Not Implemented");
+            Console.ResetColor();
+            return -1;
+        }
+
+
+        public abstract void ReadInputFile();
+
+    }
+}
